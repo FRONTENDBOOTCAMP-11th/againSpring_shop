012fd57ec986c46560b9cc76d111ad26acf0264f
jiyoungnim <cloundyon31@gmail.com>
2025-01-02 17:20:20 +0900

    💄 테일윈드CSS 적용

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,45 +1,49 @@
 import "./main.css";
+import "./jy-global.css";
 
 function Login() {
   return (
     <>
       <div className="my-12 mx-auto">
-        <div className="content">
-          <div className="login_logo_wrap">
+        <div className="w-[400px] mx-auto flex flex-col items-center p-[20px_40px_44px] border border-grey-20 rounded-[20px] gap-[44px]">
+          <div className="w-16 aspect-[1/1] object-contain">
             <img src="/public/favicon.png" alt="logo" />
           </div>
 
-          <div className="login_wrap">
+          <div className="w-[320px]">
             <form>
-              <div className="userInput">
-                <div className="field">
+              <div className="flex flex-col gap-3">
+                <div className="border-2 border-grey-10 rounded-2xl focus-within:border-secondary-20">
                   <input id="email" type="text" placeholder="아이디" />
                 </div>
 
-                <div className="field">
+                <div className="border-2 border-grey-10 rounded-2xl focus-within:border-secondary-20">
                   <input id="password" type="password" placeholder="비밀번호" />
                 </div>
               </div>
 
-              <div className="btn-area">
-                <button className="button btn-login">로그인</button>
-                <button className="button btn-kakao">
+              <div className="mt-8">
+                <button className="font-gowunBold w-full h-[42px] text-[1.6rem] rounded-[12px] text-center cursor-pointer box-border bg-primary-40 text-white mb-[10px]">
+                  로그인
+                </button>
+                <button className="font-gowunBold w-full h-[42px] text-[1.6rem] rounded-[12px] text-center cursor-pointer box-border bg-kakao text-black">
                   카카오톡으로 시작하기
                 </button>
               </div>
 
-              <label className="choice">
+              <label className="choice flex gap-3 mt-8 align-middle">
                 <input type="checkbox" />
                 자동 로그인
               </label>
 
-              <ul className="find_wrap">
-                <li className="signUp">
-                  <a>회원가입</a> ｜
+              <ul className="font-gowun mt-12 text-center">
+                <li>
+                  <a className="cursor-pointer text-primary-70">회원가입</a> ｜
                 </li>
                 <li>
-                  <div className="find_text">
-                    <a>아이디</a> · <a>비밀번호</a> 찾기
+                  <div className="inline-block">
+                    <a className="cursor-pointer underline">아이디</a> ·{" "}
+                    <a className="cursor-pointer underline">비밀번호</a> 찾기
                   </div>
                 </li>
               </ul>
