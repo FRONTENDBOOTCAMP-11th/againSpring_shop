f469c4de6dbaea65df515c4f6682a3ec55b87c30
jiyoungnim <cloundyon31@gmail.com>
2025-01-02 23:18:03 +0900

    💄 테일윈드CSS 수정...

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -12,30 +12,41 @@ function Login() {
           <div className="w-[320px]">
             <form>
               <div className="flex flex-col gap-3">
-                <div className="border-2 border-grey-10 rounded-2xl focus-within:border-secondary-20">
+                <div className="border-2 border-grey-10 rounded-2xl focus-within:border-secondary-20 px-4">
                   <input id="email" type="text" placeholder="아이디" />
                 </div>
 
-                <div className="border-2 border-grey-10 rounded-2xl focus-within:border-secondary-20">
+                <div className="border-2 border-grey-10 rounded-2xl focus-within:border-secondary-20 px-4">
                   <input id="password" type="password" placeholder="비밀번호" />
                 </div>
               </div>
 
               <div className="mt-8">
-                <button className="font-gowunBold w-full h-[42px] text-[1.6rem] rounded-[12px] text-center cursor-pointer box-border bg-primary-40 text-white mb-[10px]">
+                <button className="font-gowunBold w-full h-[42px] text-[1.6rem] rounded-[12px] text-center cursor-pointer box-border bg-primary-40 text-white mb-[10px] focus-within:bg-primary-30">
                   로그인
                 </button>
-                <button className="font-gowunBold w-full h-[42px] text-[1.6rem] rounded-[12px] text-center cursor-pointer box-border bg-kakao text-black">
+                <button className="font-gowunBold w-full h-[42px] text-[1.6rem] rounded-[12px] text-center cursor-pointer box-border bg-kakao text-black focus-within:bg-kakao-hover">
                   카카오톡으로 시작하기
                 </button>
               </div>
 
-              <label className="choice flex gap-3 mt-8 align-middle">
-                <input type="checkbox" />
+              {/* <label
+                htmlFor="checkbox"
+                className="flex items-center gap-3 mt-8 before:w-[20px] before:h-[20px] before:inline-block before:content-[''] before:bg-[url('./icons/emptybox.svg')] peer-checked:before:bg-[url('./icons/checkbox.svg')]"
+              >
+                <input type="checkbox" id="checkbox" className="peer" />
+                자동 로그인
+              </label> */}
+
+              <input type="checkbox" id="checkbox" className="peer" />
+              <label
+                htmlFor="checkbox"
+                className="flex items-center gap-3 mt-8 before:w-[20px] before:h-[20px] before:inline-block before:content-[''] before:bg-[url('./icons/emptybox.svg')] peer-checked:before:bg-[url('./icons/checkbox.svg')]"
+              >
                 자동 로그인
               </label>
 
-              <ul className="font-gowun mt-12 text-center">
+              <ul className="font-gowun mt-14 text-center">
                 <li>
                   <a className="cursor-pointer text-primary-70">회원가입</a> ｜
                 </li>
