bf38507be9df1c769bf67270b5322a919ad42c5c
민정 <minjaung5073@naver.com>
2025-01-08 03:05:09 +0900

    상품상세 페이지 마크업 및 테일윈드적용

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import useMenuStore from "../store/menuStore";
 
 function Cart() {
   //더미 상품 데이터
   const [activeTab, setActiveTab] = useState("가"); // 기본 활성 탭은 '가'
+  const { state } = useLocation(); // navigate로 전달된 데이터
+  const { id } = useParams(); // URL의 파라미터 값
 
   const tabContent = {
     상세정보: (
@@ -52,7 +55,7 @@ function Cart() {
   };
 
   const dummyItems = [
-    {
+    state || {
       id: 1,
       name: "상품 A",
       price: 15000,
@@ -141,7 +144,24 @@ function Cart() {
         <div>
           {/*🦋🍓 장바구니에 아이템이 있을 때 */}
           {cartItemsList.map((item) => (
-            <div className="flex ml-[100px] mt-[50px]">
+            <div className="flex ml-[80px] mt-[50px]">
+              <div className="flex flex-col  mr-[40px] ">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-[100px] h-[110px] mb-[30px] object-cover mr-[100px]"
+                />
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-[100px] h-[110px] mb-[30px] object-cover mr-[100px]"
+                />
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-[100px] h-[110px] mb-[20px] object-cover mr-[100px]"
+                />
+              </div>
               <img
                 src={item.image}
                 alt={item.name}
@@ -228,13 +248,13 @@ function Cart() {
                       </dd>
                     </div>
                     <div className="flex mb-[16px] mt-[70px] ">
-                      <button className="bg-secondary-10 border-2 border-black  w-[200px] py-[20px] mr-[10px] rounded-md text-[15px] text-center hover:bg-secondary-20 flex justify-center items-center">
+                      <button className="bg-white border-2 border-gray-300  w-[200px] py-[20px] mr-[10px] rounded-md text-[15px] text-center hover:bg-secondary-20 flex justify-center items-center">
                         찜하기
                       </button>
-                      <button className="bg-secondary-10 border-black border-4 w-[200px] py-[20px] mr-[10px] rounded-md text-[15px] text-center hover:bg-secondary-20 flex justify-center items-center">
+                      <button className="bg-white border-gray-300 border-2 w-[200px] py-[20px] mr-[10px] rounded-md text-[15px] text-center hover:bg-secondary-20 flex justify-center items-center">
                         장바구니
                       </button>
-                      <button className="bg-secondary-10 border-gray-500 border-1 w-[200px] py-[20px] mr-[10px] rounded-md text-[15px] text-center hover:bg-secondary-20 flex justify-center items-center">
+                      <button className="bg-secondary-10 border-gray-300 border-2 w-[200px] py-[20px] mr-[10px] rounded-md text-[15px] text-center hover:bg-secondary-20 flex justify-center items-center">
                         구매하기
                       </button>
                     </div>
